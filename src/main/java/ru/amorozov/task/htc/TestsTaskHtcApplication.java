937b90d817127ead9979aca09b0e454bf0d20fb0
Aleksei Morozov <aleksei_morozov@epam.com>
2022-01-15 12:30:41 +0400

    Initial commit

---
+++ src/main/java/ru/amorozov/task/htc/TestsTaskHtcApplication.java
@@ -0,0 +1,13 @@
+package ru.amorozov.task.htc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsTaskHtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsTaskHtcApplication.class, args);
+	}
+
+}
